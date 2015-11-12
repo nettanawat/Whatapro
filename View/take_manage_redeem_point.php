5d9fca490d18d06044b365162e428134492a09db
nettanawat <nettanwat@Tanawats-MacBook-Pro.local>
2015-11-12 22:13:08 +0700

    add promotion page and check location

---
+++ View/take_manage_redeem_point.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nettanwat
+ * Date: 11/12/15 AD
+ * Time: 3:04 PM
+ */
+include dirname(__FILE__) . '/../Config.php';
+
+if(isset($_POST['add_redeem'])){
+    $redeemCodeController = new RedeemCodeController();
+//    $redeemCodeController->
+}
---
+++ src/dao/PointDAOImpl.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nettanwat
+ * Date: 11/12/15 AD
+ * Time: 3:25 PM
+ */
+
+class PointDAOImpl {
+
+} 
